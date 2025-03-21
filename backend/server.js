7bf38c39d302f3bba95ae6cba01cc10d335a9df6
oshadaov <oshadaov@gmail.com>
2025-03-21 10:30:56 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,7 @@ dotenv.config()
 const app = express()
 
 // Middleware
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? ["https://muscle-care-frontend.vercel.app//"]
-        : ["http://localhost:5174", "http://localhost:5173"],
-    credentials: true,
-  }),
-)
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
