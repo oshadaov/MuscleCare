2057fa0a8e32a61769917e13b5f081d751b3eec1
oshadaov <oshadaov@gmail.com>
2025-03-21 14:51:44 +0530

    admin

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL:  "http://localhost:5000/api",
+  baseURL:  apiUrl||"http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
